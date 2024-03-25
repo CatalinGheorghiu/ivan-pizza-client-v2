9c5c665aca1e7947c59b9cc6661bf3f8e34ef656
Ionut Catalin GHEORGHIU <56912330+CatalinGheorghiu@users.noreply.github.com>
2024-03-25 16:27:24 +0200

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,27 @@
+import "./index.css";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import App from "@/App.tsx";
+
+const queryClient = new QueryClient();
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  }
+]);
+
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
