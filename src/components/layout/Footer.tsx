d5d575c8077401b35d451306ffe041b4fb5e6570
Catalin <g.ionutcatalin@protonmail.com>
2024-03-27 15:18:18 +0200

    fix: footer overlay

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -4,15 +4,19 @@ import { getCurrentDate } from "@/utils";
 
 const Footer = () => {
   return (
-    <footer className="p-6 text-center text-sm font-semibold leading-6 md:text-base">
-      © Ivan&apos;s Pizza {getCurrentDate()}. Website Designed and Developed by{" "}
-      <Link
-        to="https://catalin-gheorghiu.dev/"
-        target="_blank"
-        className="text-red-500 underline underline-offset-2 outline-offset-2"
-      >
-        Catalin Gheorghiu
-      </Link>
+    <footer className="relative overflow-hidden p-6 text-center text-sm font-semibold leading-6 md:text-base">
+      <p className="relative z-20 mx-auto max-w-fit">
+        © Ivan&apos;s Pizza {getCurrentDate()}. Website Designed and Developed
+        by{" "}
+        <Link
+          to="https://catalin-gheorghiu.dev/"
+          target="_blank"
+          className="text-red-500 underline underline-offset-2 outline-offset-2"
+        >
+          Catalin Gheorghiu
+        </Link>
+      </p>
+      <div className="absolute top-1/2 z-10 h-full w-full -translate-y-1/2 overflow-hidden bg-black/50 p-4"></div>
     </footer>
   );
 };
